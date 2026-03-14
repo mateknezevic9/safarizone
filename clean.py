975f158e908e201382a344f28bcef3758635d0f5
mateknezevic9 <104849736+mateknezevic9@users.noreply.github.com>
2026-03-14 23:14:51 +0100

    Delete clean.py

--- clean.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-orig = len(html)
-html = re.sub(r'src="data:image/[^;]+;base64,[^"]{20,}"', 'src=""', html)
-html = re.sub(r'<div[^>]*class="[^"]*card[^"]*"[^>]*>(?:(?!<\/div>)[\s\S])*?[Cc]harizard(?:(?!<\/div>)[\s\S])*?<\/div>', '', html)
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(html)
-print(f"Done: {orig} -> {len(html)}")
